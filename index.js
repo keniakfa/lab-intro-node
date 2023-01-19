9981660c7ba8830e48603bb34de6361c9205a061
Kenia Araujo <kenia.kfa@gmail.com>
2023-01-19 16:02:02 -0600

    done

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 class SortedList {
-  constructor() {}
+  constructor(){
+    this.items = [];
+    this.length= this.items.length
+  };
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++
+    this.items.sort((a, b) => a - b);
+  
+    return this.items
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (!this.length || pos > this.length){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+    
+  }
 
-  min() {}
+  max() {
+    if (!this.length){
+    throw new Error('EmptySortedList');
+    }
+    return this.items[this.length-1]
+  }
 
+  min() {
+
+    if (!this.length){
+      throw new Error('EmptySortedList');
+      }
+      return this.items[0]
+    
+  }
   sum() {}
 
   avg() {}
+
+ 
 }
 
 module.exports = SortedList;
+
